152cdaacea658d9d319fc1681e71ec780a561740
StellarDevTalha <alamtalha854@gmail.com>
2025-12-03 17:27:01 +0500

    JavaScript

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello JavaScript");
+
+// Variable
+let name = "Talha";
+console.log(name);
+
+// functions
+function greet (){
+    console.log("Welcome Talha!");
+}
+greet();
+
+function sayHello(){
+    alert("Hello Talha!");
+}
